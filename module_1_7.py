57f330ed7723902c7793581cbe6f737c82351719
Богачева Анастасия <britta18britta@gmail.com>
2024-12-18 22:17:58 +0300

    Add files via upload

---
+++ module_1_7.py
@@ -0,0 +1,15 @@
+
+dict_school = {}
+
+a = [5,3,3,5,4,]
+a_1 = sum(a)/len(a)
+b = [2,2,2,3]
+b_1 = sum(b)/ len(b)
+c = [4,5,5,2]
+c_1 = sum(c)/len(c)
+d = [4,4,3]
+d_1 = sum(d)/len(d)
+e = [5,5,5,4,5]
+e_1 = sum(e)/len(e)
+dict_school.update({'Jhony': a_1, 'Bilbo': b_1, 'Steve': c_1, 'Khendrilk': d_1, 'Aaron': e_1 })
+print(dict_school)
